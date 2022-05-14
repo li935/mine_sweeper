4ea1473be64023266917b91544428856cc4831ef
donghyun_kim <bandits0726@kakao.com>
2022-05-15 05:17:40 +0900

    chore: Edit comments
    - Edit comments

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 export const CELL = {
-    OPENED: 0,      // 일반 칸 (OPEN)
+    OPENED: 0,      // 일반 칸 (OPENED)
     NORMAL: -1,     // 일반 칸 (CLOSED)
     FLAG: -2,       // 깃발 칸
-    BOOM: -3,       // 지뢰 칸 (OPEN)
+    BOOM: -3,       // 지뢰 칸 (OPENED)
     MINE: -4,       // 지뢰 칸 (CLOSED)
 };
 
